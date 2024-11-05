056b3f51f033112eb317e26f42c775c5ca9a427b
Vayde <Vaydesb@gmail.com>
2024-11-05 05:41:03 -0800

    6. Player and Enemy Characters

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,19 @@
+// Copyright Lone Pack Studio [2024]
+
+
+#include "Character/AuraCharacterBase.h"
+
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+ 	
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
---
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -0,0 +1,17 @@
+// Copyright Lone Pack Studio [2024]
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AuraCharacterBase.h"
+#include "AuraEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraEnemy : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
